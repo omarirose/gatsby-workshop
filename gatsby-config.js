654f7b41ae6ecae88fc577f49eb21e1cd371e048
omarirose <omarirose@users.noreply.github.com>
2020-07-24 15:20:25 -0400

    fixed invalid date bug

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,28 @@
 module.exports = {
-  plugins: [`gatsby-plugin-mdx`],
+  siteMetadata: {
+    title: `Omari Rose Blog`,
+    author: `Omari Rose`,
+    description: `Omari Rose personal site for Gatby workshop`,
+    siteUrl: `https://gatsby-codeland-101.netlify.app`,
+    social: [
+      {
+        name: `Twitter`,
+        url: `https://twitter.com/OmariRose`,
+      },
+      {
+        name: `Github`,
+        url: `https://www.github.com/omarirose`,
+      },
+    ],
+  },
+  plugins: [
+    `gatsby-plugin-mdx`,
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {
+        basePath: `/blog`,
+        mdxOtherwiseConfigured: true,
+      },
+    },
+  ],
 };
